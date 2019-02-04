8c4db6b8cac9c055b9cea6445c730cce6d532ba9
Bianca <ing_biank@hotmail.com>
2019-02-04 12:07:44 +0100

    Accessing tag attributes through props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>I'm a react app </h1>
-        <Person />
+        <Person name="John" age="25"/>
+        <Person name="Smith" age="29">Hobbies: Paint</Person>
+        <Person name="John" age="25"/>
       </div>
     );
   }
