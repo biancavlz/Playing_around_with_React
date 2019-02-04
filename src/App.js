f0067dfce3cc03530fb24b4dc782aee5b733fead
Bianca <ing_biank@hotmail.com>
2019-02-04 12:54:51 +0100

    using a state property, and accessing the values through .this

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "john", age: "25" },
+      { name: "Smith", age: "29"  },
+      { name: "jane", age: "25" }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>I'm a react app </h1>
-        <Person name="John" age="25"/>
-        <Person name="Smith" age="29">Hobbies: Paint</Person>
-        <Person name="John" age="25"/>
+        <button>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Paint</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
