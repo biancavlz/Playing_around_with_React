b96b40df2a74e63fe83916fc14446fd0dbc0e993
Bianca <ing_biank@hotmail.com>
2019-02-11 12:36:13 +0100

    Getting the value of the input, and rendering it dynamically

--- src/App.js
+++ src/App.js
@@ -23,13 +23,28 @@ class App extends Component {
     )
   }
 
+  nameChangeHandler = (event) => {
+    this.setState(
+      {
+        persons: [
+          { name: "Max", age: "25" },
+          { name: event.target.value, age: "29"  },
+          { name: "jane", age: "30" }
+        ]
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <h1>I'm a react app </h1>
-        <button onClick={ this.switchNameHandle.bind(this, "Maximilian") }>Switch name</button>
+        <button onClick={ () => this.switchNameHandle("Maximilian") }>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person click={this.switchNameHandle.bind(this, Max!!!")} name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Paint</Person>
+        <Person click={this.switchNameHandle.bind(this, "Max!!!")}
+          changed={this.nameChangeHandler}
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}>Hobbies: Paint</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
