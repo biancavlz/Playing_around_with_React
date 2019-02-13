3554cf7ff0bbcb65734c9237a467c7fc70f1c618
Bianca <ing_biank@hotmail.com>
2019-02-13 11:51:47 +0100

    Using ternary conditional in JSX to toggle persons list

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       { name: "john", age: "25" },
       { name: "Smith", age: "29"  },
       { name: "jane", age: "25" }
-    ]
+    ],
+
+    showPersons: false
   }
 
   switchNameHandle = (newName) => {
@@ -35,6 +37,11 @@ class App extends Component {
     )
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   render() {
     const styling = {
       backgroundColor: "white", 
@@ -47,13 +54,26 @@ class App extends Component {
     return (
       <div className="App">
         <h1>I'm a react app </h1>
-        <button style= {styling} onClick={ () => this.switchNameHandle("Maximilian") }>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person click={this.switchNameHandle.bind(this, "Max!!!")}
-          changed={this.nameChangeHandler}
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}>Hobbies: Paint</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button style= {styling} onClick={ this.togglePersonsHandler }>Toggle persons</button>
+        { 
+          this.state.showPersons === true ? 
+            <div>
+              <Person 
+                name={this.state.persons[0].name} 
+                age={this.state.persons[0].age}
+              />
+              <Person 
+                click={this.switchNameHandle.bind(this, "Max!!!")}
+                changed={this.nameChangeHandler}
+                name={this.state.persons[1].name} 
+                age={this.state.persons[1].age}>Hobbies: Paint
+              </Person>
+              <Person 
+                name={this.state.persons[2].name} 
+                age={this.state.persons[2].age}
+              />
+            </div> : null
+        }
       </div>
     );
   }
